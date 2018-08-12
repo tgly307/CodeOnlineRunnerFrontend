6a60c5e6aed9a80680742c7770a00def11a8e076
tgly307 <tgly307@163.com>
2018-08-12 22:55:56 +0800

    lint

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,3 @@
-const path = require('path');
-function resolve (dir) {
-    return path.join(__dirname,dir)
-}
-
 module.exports = {
-    runtimeCompiler: true,
-    // configureWebpack: config => {
-    //     config.resolve = {
-    //         extensions: ['.js', '.vue', '.json',".css"],
-    //         alias: {
-    //             'vue$': 'vue/dist/vue.esm.js',
-    //             '@': resolve('src'),
-    //         }
-    //     }
-    // },
+  runtimeCompiler: true
 };
