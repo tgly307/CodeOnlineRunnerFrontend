49ae4e7b57d6c02e520d3849f0dd34d5ca0cba8a
tgly307 <tgly307@163.com>
2018-08-19 02:10:41 +0800

    fix code style

--- src/router/index.js
+++ src/router/index.js
@@ -13,17 +13,20 @@ export default new Router({
     {
       path: "/node",
       name: "node",
-      component: () => import(/* webpackChunkName: "node" */ "@/views/node/node")
+      component: () =>
+        import(/* webpackChunkName: "node" */ "@/views/node/node")
     },
     {
       path: "/python",
       name: "python",
-      component: () => import(/* webpackChunkName: "python" */ "@/views/python/python")
+      component: () =>
+        import(/* webpackChunkName: "python" */ "@/views/python/python")
     },
     {
       path: "/c_cpp",
       name: "c_cpp",
-      component: () => import(/* webpackChunkName: "c_cpp" */ "@/views/c_cpp/c_cpp")
+      component: () =>
+        import(/* webpackChunkName: "c_cpp" */ "@/views/c_cpp/c_cpp")
     }
   ]
 });
